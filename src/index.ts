57d2737aacbf8e80225e5701e2066bef36790f4a
pcan <10360978+pcan@users.noreply.github.com>
2025-11-03 15:55:48 +0100

    cleaning up old version

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,9 @@
-import { Client } from "./client.ts";
-import { type Deserialize, type Sql, sqlFactory } from "./sql.ts";
-
+export * from "./api/client.ts"
+export * from "./api/ppg.ts"
 export {
-  Client,
-  type ClientOptions,
-  RequestError,
-  SqlError,
-} from "./client.ts";
-export type { Column, QueryResponse } from "./queryable.ts";
-export { ConnectionStringError } from "./url.ts";
-export type { Deserialize, Sql } from "./sql.ts";
-
-/**
- * Connects to the specified Prisma Postgres database and returns a high-level
- * SQL client provided as a template literal tag function.
- *
- * ```ts
- * const sql = ppg("prisma+postgres://accelerate.prisma-data.net/?api_key=...");
- * const posts: Post[] = await sql`SELECT * FROM posts WHERE user_id = ${userId}`;
- * ```
- *
- * The interpolated values are automatically converted to SQL parameters to
- * prevent SQL injection attacks.
- *
- * You can also pass a custom deserializer function to convert the values based
- * on the column type.
- *
- * See also {@link Client} for the low-level client API.
- */
-export function ppg(
-  url: string,
-  deserialize: Deserialize = (value) => value,
-): Sql {
-  const client = new Client({ connectionString: url });
-  return sqlFactory(client, deserialize);
-}
-
-export default ppg;
+  BoundedByteStreamParameter, ByteArrayParameter, CollectableIterator,
+  DatabaseError, ValidationError, WebSocketError,
+  DatabaseErrorDetails, HttpResponseError, GenericError, boundedByteStreamParameter,
+  byteArrayParameter, BINARY, TEXT, ParameterFormat,
+} from "./common/types.ts"
+export { Column } from "./transport/shared.ts"
